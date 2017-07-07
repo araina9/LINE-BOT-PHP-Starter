3d5180842858993f5ab1b8c33ea15906859537f5
araina9 <araina_9@hotmail.com>
2017-07-07 16:08:32 +0700

    Update bot1.php

--- bot1.php
+++ bot1.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message') {
 			// Get text sent
 			//$text = $event['source']['userId'];
-			$text = $event['source']['userId'] . $event['message']['type'];
+			$text = $event['source']['userId'] ."\n". $event['message']['type'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
