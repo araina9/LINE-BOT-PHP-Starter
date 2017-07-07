8961bc357427a5e3275c9bf405b50a2f032713d0
araina9 <araina_9@hotmail.com>
2017-07-07 09:58:56 +0700

    Verify1
    init

---
+++ Verify1.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = 'JyHZe860ykSn2FufkTtH4DMEL3YhZd0egdsfVgNfF3moQSSFr4Kgo/K01vXzGzGdqXSYFFX/l1Y3yKJqIuZ1DLuRzmJrmyAH9toE32q3j5K1wVnlPQZhowfpJemzE5kSlRKHSiyE7quuYD76T5u5QgdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
